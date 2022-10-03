b099f91fdbcfbca67bd58f931b001d17ac6dad49
shl518 <71417331+shl518@users.noreply.github.com>
2022-10-03 16:13:38 +0800

    Initial Commit

---
+++ SUIHangTaste/SUIHangTasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIHangTasteApp.swift
+//  SUIHangTaste
+//
+//  Created by Shl on 2022/10/3.
+//
+
+import SwiftUI
+
+@main
+struct SUIHangTasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
